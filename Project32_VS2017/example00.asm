16e583013b2c8f69e51976fa33483cb9e6f5bb55
supernimbus <mariofm.mx@gmail.com>
2017-09-26 00:52:19 -0700

    Initial commit for CS271 repository containing first example

---
+++ Project32_VS2017/example00.asm
@@ -0,0 +1,37 @@
+TITLE Dog Years     (Project00.asm)
+
+; Author: Mario			
+; Course / Project ID	:	DEMO #0            Date: 9/26/2017
+; Description: This program will introduce the programmer, get the user's name and age,
+;	calculate the user's "dog age", and report the result.
+
+INCLUDE Irvine32.inc
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+;Introduce programmer
+
+;Get user name
+
+;Get user age
+
+;Calculate user "dog years"
+
+;Report result
+
+;Say "Good-bye"
+; (insert executable instructions here)
+
+	exit	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
