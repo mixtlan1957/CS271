fa3f4d5c4f8cd475c60a115a09844205cd09935f
supernimbus <mariofm.mx@gmail.com>
2017-09-26 11:43:41 -0700

    First initial commit

---
+++ Project1.asm
@@ -0,0 +1,27 @@
+TITLE Program Template     (template.asm)
+
+; Author: Mario Franco-Munoz		francomm@oregonstate.edu	
+; CS271-400 / Project #1			Date: 09/26/2017
+; Description: This program will display the author and program title on the output screen and prompt the user
+;	for two numbers. The sum, difference, product, (integer) quotient and remainder of the numbers is then calculated
+;	and terminating message is displayed.
+
+INCLUDE Irvine32.inc
+
+; (insert constant definitions here)
+
+.data
+
+
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	exit	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
